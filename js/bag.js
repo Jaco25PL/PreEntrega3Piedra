7df094363942b3ba755e80ca23b8f8d1c027fc86
PiedraL <joaquinlangone12@gmail.com>
2023-06-19 19:41:01 -0300

    testing in GitHub

--- js/bag.js
+++ js/bag.js
@@ -1,51 +1,53 @@
 
-// const showBag = document.querySelector("#showBag");
-// const bagAmount = document.querySelector("#totalAmount");
+const showBag = document.querySelector("#showBag");
+const bagAmount = document.querySelector("#totalAmount");
 
 
 // setProducts();
 // getProducts(); 
 
-// getProductsBag();   
-// renderBagProducts()
-// total()
+getProductsBag();   
+renderBagProducts()
+total()
+
+function getProductsBag() {
+    return JSON.parse(localStorage.getItem("allBagProducts")) || [];
+}
+
+console.log(getProductsBag());
+
 
 // // function getProductsBag() {
 //     // return JSON.parse(localStorage.getItem("allBagProducts")) || [];
 // // }
 
 
-// function renderBagProducts(){
-//     const bag = getProductsBag();
-//     let render = "";
+function renderBagProducts(){
+    const bag = getProductsBag();
+    let render = "";
 
-//     bag.forEach(product => {
-//         render += `
-//         <div class="bag__element">
-//             <img class="bag__element-img" src="${product.img}" alt="${product.brand}}">
-//             <div class="bag__element-name"><p>${product.brand} ${product.model}</p></div>
-//             <div class="bag__element-price"><p>$${product.price}</p><span>USD</span></div>
-//             <button class="btn btn-remove" type="button" >Remover del Carrito</button>
-//         </div>`
-//     });
+    bag.forEach(product => {
+        render += `
+        <div class="bag__element">
+            <img class="bag__element-img" src="${product.img}" alt="${product.brand}}">
+            <div class="bag__element-name"><p>${product.brand} ${product.model}</p></div>
+            <div class="bag__element-price"><p>$${product.price}</p><span>USD</span></div>
+            <button class="btn btn-remove" type="button" >Remover del Carrito</button>
+        </div>`
+    });
 
-//     showBag.innerHTML = render;
-// }
+    showBag.innerHTML = render;
+}
 
-// function total() {
-//     const bag = getProductsBag();
-//     let totalAmount = 0;
+function total() {
+    const bag = getProductsBag();
+    let totalAmount = 0;
 
-//     bag.forEach(item => {
-//         totalAmount += item.price;
-//     });
+    bag.forEach(item => {
+        totalAmount += item.price;
+    });
 
-//     bagAmount.innerHTML = `<b>$${totalAmount}</b> `;
-// }
+    bagAmount.innerHTML = `<b>$${totalAmount}</b> `;
+}
 
 
-// function getProductsBag() {
-//     return JSON.parse(localStorage.getItem("allBagProducts")) || [];
-// }
-
-// console.log(getProductsBag());
